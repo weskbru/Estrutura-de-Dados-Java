3be459ab15fed1aac71205e68b18209a0b6ab179
Wesley <Wesley@DESKTOP-BH6M3VH>
2024-01-22 21:19:22 -0300

    Criando um simples array notas para armazena as notas de um aluno

---
+++ EstruturaDeDados/src/ExemploArrayList/ArrayVetor.java
@@ -0,0 +1,19 @@
+package ExemploArrayList;
+
+
+public class ArrayVetor {
+	public static void main(String[] args) {
+		
+		
+		double[] notas = new double[4];
+		
+		notas[0] = 10.0;
+		notas[1] = 4.0;
+		notas[2] = 9.4;
+		notas[3] = 3.4;
+		
+		
+		System.out.println(notas);
+		
+	}
+}
